7f7934abb4f694eab28ee13dcd9d0936f151267a
Виктор Отрощенко <v.otroschenko@mail.ru>
2020-02-12 22:33:56 +0300

    Задание 2

---
+++ Task2.py
@@ -0,0 +1,5 @@
+def my_func(name, surname, date, city, email, telephone):
+    name = input('Введите ваше имя: ')
+    surname = input('Введите свою фамилию: ')
+    date = input('Введите вашу дату рождения: ')
+    city = input
