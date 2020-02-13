7197ddc046cd39468d02a0375d45031293d84c2f
Виктор Отрощенко <v.otroschenko@mail.ru>
2020-02-13 22:37:25 +0300

    Задание 2

--- Task2.py
+++ Task2.py
@@ -1,5 +1,11 @@
-def my_func(name, surname, date, city, email, telephone):
-    name = input('Введите ваше имя: ')
-    surname = input('Введите свою фамилию: ')
-    date = input('Введите вашу дату рождения: ')
-    city = input
+name = input('Введите ваше имя: ').title()
+surname = input('Введите вашу фамилию: ').title()
+date = input('Введите вашу дату рождения: ')
+city = input('Введите ваш город: ').title()
+email = input('Введите ваш email: ')
+telephone = input('Введите ваш телефон: ')
+
+def my_history(name, surname, date, city, email, telephone):
+    return' '.join([name, surname, date, city, email, telephone])
+
+print(my_history(name, surname, date, city, email, telephone))
